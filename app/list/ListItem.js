fc63d2b5d9b95fc11d93fb764af5025fdd70bccb
CodingStorageSoo <chlqudtn347@gmail.com>
2023-08-26 01:20:02 -0700

    Edit

---
+++ app/list/ListItem.js
@@ -0,0 +1,23 @@
+"use client";
+
+import Link from "next/link";
+
+export default async function ListItem(props) {
+  return (
+    <div className="list-item">
+      <Link href={`/detail/${props.result._id}`}>
+        <h4>{result.title}</h4>
+      </Link>
+      <Link href={`/edit/${props.result._id}`}>📝</Link>
+
+      <span
+        onClick={() => {
+          fetch("/url").then(() => {});
+        }}
+      >
+        🚮
+      </span>
+      <p>{result.content}</p>
+    </div>
+  );
+}
